b10a301fae4766c74709eaa4c24ded40175bf2f6
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-02-13 15:55:55 +0100

    le ponemos cosas guapas a la interfaz

--- interfaz.py
+++ interfaz.py
@@ -8,9 +8,22 @@ raiz.resizable(1, 1)
 
 raiz.iconbitmap("UAX.ico")
 
-raiz.geometry("650x350")
 
-raiz.config(bg="grey")
+raiz.config(bg="black")
+
+miFrame=Frame()
+
+miFrame.pack(fill="both", expand="True")
+
+miFrame.config(bg="grey")
+
+miFrame.config(width="650", height="350")
+
+miFrame.config(bd=35)
+
+miFrame.config(relief="groove")
+
+miFrame.config(cursor="pirate")
 
 raiz.mainloop()
 
