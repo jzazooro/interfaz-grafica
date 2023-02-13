a01bd001184140f11893dbed9566abd665f5871e
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-02-13 15:38:49 +0100

    interfaz grafica

---
+++ interfaz.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+raiz=Tk()
+
+raiz.title("Interfaz grafica")
+
+raiz.resizable(1, 1)
+
+raiz.iconbitmap("UAX.ico")
+
+raiz.geometry("650x350")
+
+raiz.config(bg="grey")
+
+raiz.mainloop()
+
