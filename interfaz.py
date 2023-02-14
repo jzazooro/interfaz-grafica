cedd8edbafd32e01d236e92fdb5a35e44465d15a
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-02-14 03:01:20 +0100

    changes bro

--- interfaz.py
+++ interfaz.py
@@ -4,20 +4,17 @@ raiz=Tk()
 
 raiz.title("Interfaz grafica")
 
-raiz.resizable(1, 1)
-
 raiz.iconbitmap("UAX.ico")
 
-
-raiz.config(bg="black")
+raiz.config(bg="blue")
 
 miFrame=Frame()
 
-miFrame.pack(fill="both", expand="True")
+miFrame.pack(fill="y", expand="True")
 
 miFrame.config(bg="grey")
 
-miFrame.config(width="650", height="350")
+miFrame.config(width="1200", height="600")
 
 miFrame.config(bd=35)
 
@@ -25,5 +22,14 @@ miFrame.config(relief="groove")
 
 miFrame.config(cursor="pirate")
 
-raiz.mainloop()
+Label(miFrame, text="Practica de interfaz grafica", font= ("Arial", 20)).place(x=400, y=30)
 
+Label(miFrame, text="Nombre y apellidos: ", font=("Arial", 12)).place(x=175, y=150)
+
+textouno=Entry(miFrame)
+
+textouno(font=("Arial", 12))
+
+textouno.place(x=350, y=150)
+
+raiz.mainloop()
